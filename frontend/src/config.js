cf2860997f043da327ee0f2f773c956706d52188
Chintaluri Akash <akashchintaluriofficial@gmail.com>
2025-04-12 21:31:19 +0530

    Fixing

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-const BASE_URL = process.env.REACT_APP_API_BASE_PATH || '/.netlify/functions';
+const BASE_URL = import.meta.env.VITE_API_BASE_PATH || '/.netlify/functions';
 export default BASE_URL;
